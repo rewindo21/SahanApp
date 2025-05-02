f9e89410dfc5b155367a369dd9254f0aba73d03a
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2025-05-02 15:09:18 +0330

    ###

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,7 +1,5 @@
-export const dynamic = "force-dynamic";
-
 import OpenAi from 'openai'
 
 export const openai = new OpenAi({
-  apiKey: process.env.OPEN_AI_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 })
