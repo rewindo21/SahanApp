3630407ecdfd16925e792bd1a13e9a19dd5c1a15
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2025-05-04 00:43:36 +0330

    #####

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,19 +7,30 @@ import {
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/api/payment(.*)",
-  "callback(.*)",
+  "/callback(.*)",
 ]);
 
-// redirect to sign-in when user wants to access the dashboard
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
-});
+// // redirect to sign-in when user wants to access the dashboard
+// export default clerkMiddleware(async (auth, req) => {
+//   if (isProtectedRoute(req)) await auth.protect();
+// });
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
+export default clerkMiddleware(async (auth, req) => {
+  const url = req.nextUrl.pathname;
+
+  // Avoid redirect loop on public auth routes
+  if (url.startsWith("/sign-in") || url.startsWith("/sign-up")) return;
+
+  if (isProtectedRoute(req)) {
+    await auth().protect();
+  }
+});
